296f1fe2a8359eb79d444bd98a11ba43bec3396c
Athul V L <103096494+athulvl@users.noreply.github.com>
2022-10-11 18:30:07 +0530

    Update settings.py

--- Admin/settings.py
+++ Admin/settings.py
@@ -22,7 +22,7 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!nf$dm(7598yio!56i$@nmgxu4p0qam@)*m=(vf6!w93gxblm#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
